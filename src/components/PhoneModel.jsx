06bbbd4c6db000265f7d0c178a77509fa12573f0
AbsharHassan <abshar.hassan7@gmail.com>
2023-10-12 15:27:24 +0500

    phoneModel made responsive. Removed from contentful

--- src/components/PhoneModel.jsx
+++ src/components/PhoneModel.jsx
@@ -1,12 +1,23 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Vector3 } from 'three'
-import { useFrame } from '@react-three/fiber'
+import { useFrame, useThree } from '@react-three/fiber'
 
 import gsap from 'gsap'
 
 import IPhone11Model from './IPhone11Model'
 
 const PhoneModel = ({ vec = new Vector3(), fullView, setFullView }) => {
+  const { viewport } = useThree()
+
+  const [isSmallViewport, setIsSmallViewport] = useState(
+    viewport.width <= 2.398349003611367
+  )
+
+  const [previousViewport, setPreviousViewport] = useState({
+    width: 0,
+    height: 0,
+  })
+
   let phoneMeshRef = useRef(null)
   let mousePos = useRef({ x: -1, y: 0.5 })
   let lookAtVector = useRef(new Vector3(-10, 0, 0))
@@ -30,14 +41,26 @@ const PhoneModel = ({ vec = new Vector3(), fullView, setFullView }) => {
   }, [])
 
   useEffect(() => {
-    gsap.to(phoneMeshRef.current.position, {
-      x: fullView ? 0 : 0.6,
-      z: fullView ? 0.5 : 0,
-    })
-    gsap.to(frontPointLightRef.current.position, {
-      y: fullView ? 2 : 0,
-    })
-  }, [fullView])
+    if (!isSmallViewport) {
+      gsap.to(phoneMeshRef.current.position, {
+        x: fullView ? 0 : 0.6,
+        z: fullView ? 0.5 : 0,
+        y: 0,
+      })
+      gsap.to(frontPointLightRef.current.position, {
+        y: fullView ? 2 : 0,
+      })
+    } else {
+      gsap.to(phoneMeshRef.current.position, {
+        x: 0,
+        z: 0,
+        y: 0.25,
+      })
+      gsap.to(frontPointLightRef.current.position, {
+        y: fullView ? 2 : 0,
+      })
+    }
+  }, [fullView, isSmallViewport])
 
   useFrame(() => {
     if (!fullView) {
@@ -59,6 +82,42 @@ const PhoneModel = ({ vec = new Vector3(), fullView, setFullView }) => {
       )
     }
   })
+
+  useEffect(() => {
+    setIsSmallViewport(viewport.width <= 2.398349003611367)
+
+    console.log(viewport.width)
+
+    if (
+      // viewport.width !== previousViewport.width ||
+      // viewport.height !== previousViewport.height
+      true
+    ) {
+      if (viewport.width > 2.88164350386745) {
+        phoneMeshRef.current.scale.x = 0.3
+        phoneMeshRef.current.scale.y = 0.3
+        phoneMeshRef.current.scale.z = 0.3
+      } else if (viewport.width < 2.398349003611367) {
+        if (viewport.width < 1) {
+          let scale = 0.15 * viewport.width > 0.1 ? 0.15 * viewport.width : 0.1
+
+          phoneMeshRef.current.scale.x = scale
+          phoneMeshRef.current.scale.y = scale
+          phoneMeshRef.current.scale.z = scale
+        } else {
+          phoneMeshRef.current.scale.x = 0.15
+          phoneMeshRef.current.scale.y = 0.15
+          phoneMeshRef.current.scale.z = 0.15
+        }
+      } else {
+        phoneMeshRef.current.scale.x = 0.3
+        phoneMeshRef.current.scale.y = 0.3
+        phoneMeshRef.current.scale.z = 0.3
+      }
+    }
+
+    setPreviousViewport({ width: viewport.width, height: viewport.height })
+  }, [viewport])
 
   return (
     <>
